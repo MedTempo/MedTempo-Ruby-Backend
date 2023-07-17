86175e60220825ae7167fe154761ca76c7cf1d13
Mateus Felipe Vieira <habdig7@gmail.com>
2023-07-17 17:20:48 -0300

    Datastax Connect

--- config/server.rb
+++ config/server.rb
@@ -1,11 +1,31 @@
 require "sinatra/base"
+=begin
+
+    Copyright © 2023 Felipe Chiozzotto Gozzani, Heloísa Real, Juliana Barbosa Sandes, Mateus Felipe da Silveira Vieira, Thiago Babtista da Silva Soares
+
+
+    MedTempo-Backend++ is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Affero General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    MedTempo-Backend++ is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Affero General Public License for more details.
+
+    You should have received a copy of the GNU Affero General Public License
+    along with MedTempo-Backend++.  If not, see <https://www.gnu.org/licenses/>5.
+    
+=end
+
 require "./config/cors"
 require "./config/router"
 
 module ServerConfig
     def self.defaults
 
-         Sinatra::Application::set :environment, :production
+        # Sinatra::Application::set :environment, :production
 
        if Sinatra::Application::production?
         Sinatra::Application::set :logging, false
